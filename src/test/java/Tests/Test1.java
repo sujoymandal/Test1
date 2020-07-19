b0aa6940b621e0848a12aabac68426c0384e21fa
unknown <sujoymandal33@gmail.com>
2020-07-19 19:48:21 +0530

    first commit

---
+++ src/test/java/Tests/Test1.java
@@ -0,0 +1,20 @@
+package Tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Test1 {
+	
+	@Test
+	public void launchGoogle(){
+		
+		WebDriver driver=new ChromeDriver();
+		driver.manage().deleteAllCookies();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com/");
+		System.out.println(driver.getTitle());
+		driver.quit();
+	}
+
+}
